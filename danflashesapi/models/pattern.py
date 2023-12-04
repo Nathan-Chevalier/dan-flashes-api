50368eea057ea8c74a029be2a9decc5b84f3c4f8
Nathan Chevalier <cerberusgfx@gmail.com>
2023-12-04 11:45:07 -0600

    Pattern model created

---
+++ danflashesapi/models/pattern.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Pattern(models.Model):
+    pattern_url_a = models.URLField(null=False, blank=False)
+    pattern_url_b = models.URLField(null=False, blank=False)
+    label = models.CharField(max_length=64)
