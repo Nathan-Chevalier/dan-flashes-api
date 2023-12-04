9f9777a47e075e41c33a2fb7757f45291f84c4cc
Nathan Chevalier <cerberusgfx@gmail.com>
2023-12-04 14:42:58 -0600

    ShirtPattern migration

--- danflashesapi/models/shirtPattern.py
+++ danflashesapi/models/shirtPattern.py
@@ -3,4 +3,4 @@ from django.db import models
 class ShirtPattern(models.Model):
     shirt = models.ForeignKey('Shirt', on_delete=models.CASCADE)
     pattern = models.ForeignKey('Pattern', on_delete=models.CASCADE)
-    pattern_index = models.SmallIntegerField()
+    pattern_index = models.SmallIntegerField(null=True)
